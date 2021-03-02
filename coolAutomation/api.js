223ff450cf23d135b0d4eecafcf4ce07a29a5675
Nitay Ben-Zvi <nitaybz@gmail.com>
2021-03-02 19:02:16 +0200

    fix Fahrenheit for #1

--- coolAutomation/api.js
+++ coolAutomation/api.js
@@ -58,10 +58,10 @@ const sendCommand = function (cmd) {
 
 const parseState = (data) => {
 	return data.map(d => {
-
+		
 		// verify temp unit
 		let celsius = true
-		if (d.substring(11, 16).includes('F'))
+		if (d.substring(11, 17).includes('F'))
 			celsius = false
 
 		return {
@@ -69,11 +69,11 @@ const parseState = (data) => {
 			onoff: d.substring(7, 10).trim(),
 			st: parseFloat(celsius ? d.substring(11, 15).trim() : d.substring(11, 16).trim()),
 			rt: parseFloat(celsius ? d.substring(17, 21).trim() : d.substring(18, 23).trim()),
-			fspeed: celsius ? d.substring(23, 27).trim().toUpperCase() : d.substring(24, 28).trim().toUpperCase(),
-			mode: celsius ? d.substring(28, 32).trim().toUpperCase() : d.substring(29, 33).trim().toUpperCase(),
-			flr: celsius ? d.substring(33, 36).trim() : d.substring(34, 37).trim(),
-			filt: celsius ? d[38] : d[37],
-			dmnd: celsius ? d[40] : d[39],
+			fspeed: celsius ? d.substring(23, 27).trim().toUpperCase() : d.substring(25, 29).trim().toUpperCase(),
+			mode: celsius ? d.substring(28, 32).trim().toUpperCase() : d.substring(30, 34).trim().toUpperCase(),
+			flr: celsius ? d.substring(33, 36).trim() : d.substring(35, 38).trim(),
+			filt: celsius ? d[38] : d[38],
+			dmnd: celsius ? d[40] : d[40],
 			tunit: celsius ? 'C' : 'F'
 		}
 	})
