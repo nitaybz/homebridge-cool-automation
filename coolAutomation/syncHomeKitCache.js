52555ecd84f243f55372a2a790de419b9bd4f8da
Nitay Ben-Zvi <nitaybz@gmail.com>
2024-01-16 13:35:02 +0200

    remove OK accessory

--- coolAutomation/syncHomeKitCache.js
+++ coolAutomation/syncHomeKitCache.js
@@ -9,7 +9,7 @@ module.exports = (platform) => {
 			
 			// Add AirConditioner
 			const airConditionerIsNew = !platform.activeAccessories.find(accessory => accessory.type === 'AirConditioner' && accessory.id === device.uid)
-			if (airConditionerIsNew) {
+			if (airConditionerIsNew && device.uid !== 'OK') {
 				const airConditioner = new AirConditioner(device, platform)
 				platform.activeAccessories.push(airConditioner)
 			}
@@ -18,39 +18,42 @@ module.exports = (platform) => {
 
 
 		// find devices to remove
-		// const accessoriesToRemove = []
-		// platform.cachedAccessories.forEach(accessory => {
+		const accessoriesToRemove = []
+		platform.cachedAccessories.forEach(accessory => {
 
-		// 	if (!accessory.context.type) {
-		// 		accessoriesToRemove.push(accessory)
-		// 		platform.log.easyDebug('removing old cached accessory')
-		// 	}
+			// if (!accessory.context.type) {
+			// 	accessoriesToRemove.push(accessory)
+			// 	platform.log.easyDebug('removing old cached accessory')
+			// }
 
-		// 	let deviceExists
-		// 	switch(accessory.context.type) {
-		// 		case 'AirConditioner':
-		// 			deviceExists = platform.devices.find(device => device.uid === accessory.context.deviceId)
-		// 			if (!deviceExists)
-		// 				accessoriesToRemove.push(accessory)
-		// 			break
-		// 		default:
-		// 			accessoriesToRemove.push(accessory)
-		// 			break
-		// 	}
-		// })
+			// let deviceExists
+			// switch(accessory.context.type) {
+			// 	case 'AirConditioner':
+			// 		deviceExists = platform.devices.find(device => device.uid === accessory.context.deviceId)
+			// 		if (!deviceExists)
+			// 			accessoriesToRemove.push(accessory)
+			// 		break
+			// 	default:
+			// 		accessoriesToRemove.push(accessory)
+			// 		break
+			// }
 
-		// if (accessoriesToRemove.length) {
-		// 	platform.log.easyDebug('Unregistering Unnecessary Cached Devices:')
-		// 	platform.log.easyDebug(accessoriesToRemove)
+			if (accessory.displayName === 'OK AC' || accessory.name === 'OK AC' || accessory.displayName === 'OK')
+				accessoriesToRemove.push(accessory)
+		})
 
-		// 	// unregistering accessories
-		// 	platform.api.unregisterPlatformAccessories(platform.PLUGIN_NAME, platform.PLATFORM_NAME, accessoriesToRemove)
+		if (accessoriesToRemove.length) {
+			platform.log.easyDebug('Unregistering Unnecessary Cached Devices:')
+			platform.log.easyDebug(accessoriesToRemove)
 
-		// 	// remove from cachedAccessories
-		// 	platform.cachedAccessories = platform.cachedAccessories.filter( cachedAccessory => !accessoriesToRemove.find(accessory => accessory.UUID === cachedAccessory.UUID) )
+			// unregistering accessories
+			platform.api.unregisterPlatformAccessories(platform.PLUGIN_NAME, platform.PLATFORM_NAME, accessoriesToRemove)
 
-		// 	// remove from activeAccessories
-		// 	platform.activeAccessories = platform.activeAccessories.filter( activeAccessory => !accessoriesToRemove.find(accessory => accessory.UUID === activeAccessory.UUID) )
-		// }
+			// remove from cachedAccessories
+			platform.cachedAccessories = platform.cachedAccessories.filter( cachedAccessory => !accessoriesToRemove.find(accessory => accessory.UUID === cachedAccessory.UUID) )
+
+			// remove from activeAccessories
+			platform.activeAccessories = platform.activeAccessories.filter( activeAccessory => !accessoriesToRemove.find(accessory => accessory.UUID === activeAccessory.UUID) )
+		}
 	}
 }
